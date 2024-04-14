594368869e88e2ecc1ed65432ac3538c9ce7dce8
Tonyyang2525 <tonyyang2525@gmail.com>
2024-04-13 21:37:00 -0400

    initial

---
+++ script.js
@@ -0,0 +1,11 @@
+function searchF() {
+
+    let searchInput = document.getElementById('search').value;
+    //document.getElementById('') retrieves the input recieved by user.
+    //.value returns the user input 
+    //getting the element based on the id input which search. 
+    //once element is obtained, you want to get the user input, which is the .value
+    alert(searchInput);
+
+}
+
